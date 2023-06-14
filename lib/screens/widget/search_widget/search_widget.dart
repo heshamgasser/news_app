33a708be45d5bf0a18ac015cb96bd175bb045dea
Hesham Gasser <heshamgasser32@gmail.com>
2023-06-14 18:48:23 +0300

    search assignment

---
+++ lib/screens/widget/search_widget/search_widget.dart
@@ -0,0 +1,12 @@
+import 'package:anim_search_bar/anim_search_bar.dart';
+import 'package:flutter/material.dart';
+
+class SearchWidget extends StatelessWidget {
+  const SearchWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
