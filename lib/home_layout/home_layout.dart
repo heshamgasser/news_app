beb27f6264ebc17bc88a8ed6a2681884a730c66c
Hesham Gasser <heshamgasser32@gmail.com>
2023-06-15 09:40:04 +0300

    Search Text FormField

--- lib/home_layout/home_layout.dart
+++ lib/home_layout/home_layout.dart
@@ -2,6 +2,7 @@ import 'package:app_template/provider/home_provider.dart';
 import 'package:app_template/screens/category_screen.dart';
 import 'package:app_template/screens/news_screen.dart';
 import 'package:app_template/screens/widget/drawer_widget.dart';
+import 'package:app_template/screens/widget/search_widget/search_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
@@ -16,7 +17,9 @@ class HomeScreen extends StatelessWidget {
       builder: (context, child) {
         var provider = Provider.of<HomeProvider>(context);
         return Scaffold(
-          drawer: DrawerWidget(provider.onDrawerClicked,),
+          drawer: DrawerWidget(
+            provider.onDrawerClicked,
+          ),
           appBar: AppBar(
             toolbarHeight: MediaQuery.of(context).size.height * .1,
             shape: OutlineInputBorder(
@@ -27,16 +30,34 @@ class HomeScreen extends StatelessWidget {
               borderSide: BorderSide(color: Colors.transparent),
             ),
             centerTitle: true,
-            title: Text(
-              provider.categoryModel == null
-                  ? AppLocalizations.of(context)!.newsApp
-                  : provider.categoryModel!.name,
-              style: Theme.of(context).textTheme.titleLarge,
-            ),
+            title: provider.searchSelected
+                ? SearchWidget((){
+                  provider.onSearchSelected();
+            },)
+                : Text(
+                    provider.categoryModel == null
+                        ? AppLocalizations.of(context)!.newsApp
+                        : provider.categoryModel!.name,
+                    style: Theme.of(context).textTheme.titleLarge,
+                  ),
+            actions: [
+              Visibility(
+                visible: !provider.searchSelected,
+                child: IconButton(
+                  onPressed: () {
+                    provider.onSearchSelected();
+                  },
+                  icon: Icon(
+                    Icons.search,
+                    size: 30,
+                  ),
+                ),
+              ),
+            ],
           ),
           body: provider.categoryModel == null
               ? CategoryScreen(provider.onCategorySelected)
-              : NewsScreen(provider.categoryModel!) ,
+              : NewsScreen(provider.categoryModel!),
         );
       },
     );
--- lib/screens/widget/search_widget/search_widget.dart
+++ lib/screens/widget/search_widget/search_widget.dart
@@ -1,12 +1,45 @@
-import 'package:anim_search_bar/anim_search_bar.dart';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../provider/home_provider.dart';
 
 class SearchWidget extends StatelessWidget {
-  const SearchWidget({super.key});
+  Function onsearchedClosed;
+
+
+  SearchWidget(this.onsearchedClosed);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TextFormField(
+      decoration: InputDecoration(
+          filled: true,
+          fillColor: Colors.white,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20),
+          ),
+          hintText: 'Saerch Article',
+          contentPadding: EdgeInsets.symmetric(horizontal: 10),
+          prefixIcon: IconButton(
+            onPressed: () {
+              onsearchedClosed();
+            },
+            icon: Icon(Icons.close),
+          ),
+          prefixIconColor: Theme.of(context).primaryColor,
+          suffixIcon: IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+          suffixIconColor: Theme.of(context).primaryColor),
+
+    );
   }
 }
-
